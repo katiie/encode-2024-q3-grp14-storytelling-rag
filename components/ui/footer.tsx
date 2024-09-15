25aa2a6be08de60dba2daa3668ca03f31b557a6e
Pedro Orlando Acosta Pereira <poacosta87@gmail.com>
2024-09-15 15:49:48 -0600

    Adding a Footer

---
+++ components/ui/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="mt-auto py-4 text-center text-white">
+      <p>
+        &copy; {new Date().getFullYear()} Storyteller. LlamaIndex + TS + OpenAI.
+      </p>
+    </footer>
+  );
+};
+
+export { Footer };
